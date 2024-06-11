d2a651c43bb8d425b09ced767cfb4f29a5597da7
spekhy <145577204+spekhy@users.noreply.github.com>
2024-06-11 06:35:39 +0200

    tak stale nefunkcne loadovanie...

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    // component: () => import('../views/HomeView.vue')
-    component: HomeView
+    component: () => import('../views/HomeView.vue')
+    // component: HomeView
   },
   {
     path: '/about',
     name: 'about',
     component: () => import('../views/AboutView.vue')
-    // component: function () {
-    //   return import('../views/AboutView.vue')
-    // }
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,5 @@
-<!-- <script setup>
-import HomeViewComponent from './components/HomeViewComponent.vue';
-</script> -->
-<!-- src/views/HomeView.vue -->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/vue.svg">
+    <p>zase to nechce loadovat</p>
   </div>
 </template>
-
-<!-- <script>
-export default {
-  name: 'HomeViewComponentt',
-  components: {
-    HomeViewComponent,
-  }
-}
-</script> -->
