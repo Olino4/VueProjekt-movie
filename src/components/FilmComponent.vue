a18a46e29a567be52fce0a1a1ecd3ec0e6d809e4
spekhy <145577204+spekhy@users.noreply.github.com>
2024-06-12 11:54:26 +0200

    tw problem

--- src/components/FilmComponent.vue
+++ src/components/FilmComponent.vue
@@ -1,7 +1,13 @@
-<!-- <script setup>import FilmComponent from '../views/FilmComponent.vue'</script> -->
-
 <template>
-  <div class="relative mx-auto rounded-lg overflow-hidden">
-    <img src="../assets/testFilm.png" class="">
+  <div class="w-full gap-4">
+    <img src="../assets/testFilm.png" class="rounded-[24px]">
+    <div class="flex-row gap-2">
+      <h2>James Bond</h2>
+      <div class="flex-row gap-2">
+        <img src="/src/assets/time.svg">
+        <p class="w-fit">121 min</p>
+      </div>
+      <p></p>
+    </div>
   </div>
 </template>
